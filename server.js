5deb172a0b1598e6387525cdc9ac2469db0b02dc
Jonathon Blom <blomjonathon@gmail.com>
2024-02-08 09:44:24 -0600

    made changes to server.js, began adding function for adding department

--- server.js
+++ server.js
@@ -10,89 +10,129 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "jonathon123",
-    database: "departments_db",
-  },
-  console.log(`Connected to the departments_db database.`)
-);
+prompt()
 
-async function viewAllDepartments() {
-  try {
-    const departmentTable = new Table({});
+// const db = mysql.createConnection(
+//   {
+//     host: "localhost",
+//     user: "root",
+//     password: "jonathon123",
+//     database: "departments_db",
+//   },
+//   console.log(`Connected to the departments_db database.`)
+// );
 
-    const results = await new Promise((resolve, reject) => {
-      db.query("SELECT * FROM department", function (err, results) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-    results.forEach((department) => {
-      departmentTable.push([department.id, department.name]);
-    });
-    console.log(departmentTable.toString());
-    prompt();
-  } catch (error) {
-    console.error(error);
-  }
-}
-async function viewAllRoles() {
-  try {
-    const roleTable = new Table({});
+// async function viewAllDepartments() {
+//   try {
+//     const departmentTable = new Table({
+//       head: ['id', 'name']
+//     });
 
-    const results = await new Promise((resolve, reject) => {
-      db.query("SELECT * FROM role", function (err, results) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-    results.forEach((role) => {
-      roleTable.push([role.id, role.title, role.salary, role.department_id]);
-    });
-    console.log(roleTable.toString());
-    prompt();
-  } catch (error) {
-    console.error(error);
-  }
-}
-async function viewAllEmployees() {
-  try {
-    const employeeTable = new Table({});
+//     const results = await new Promise((resolve, reject) => {
+//       db.query("SELECT * FROM department", function (err, results) {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(results);
+//         }
+//       });
+//     });
+//     results.forEach((department) => {
+//       departmentTable.push([department.id, department.name]);
+//     });
+//     console.log(departmentTable.toString());
+//     prompt();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// async function viewAllRoles() {
+//   try {
+//     const roleTable = new Table({
+//       head: ['id', 'title', 'salary', 'department_id']
+//     });
 
-    const results = await new Promise((resolve, reject) => {
-      db.query("SELECT * FROM employee", function (err, results) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-    results.forEach((employee) => {
-      employeeTable.push([
-        employee.id,
-        employee.fname,
-        employee.lname,
-        employee.role_id,
-        employee.manager_id,
-      ]);
-    });
-    console.log(employeeTable)
-    console.log(employeeTable.toString());
-    prompt();
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     const results = await new Promise((resolve, reject) => {
+//       db.query("SELECT * FROM role", function (err, results) {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(results);
+//         }
+//       });
+//     });
+//     results.forEach((role) => {
+//       roleTable.push([role.id, role.title, role.salary, role.department_id]);
+//     });
+//     console.log(roleTable.toString());
+//     prompt();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// async function viewAllEmployees() {
+//   try {
+//     const employeeTable = new Table({
+//       head: ['id', 'fname', 'lname', 'role_id', 'department_id']
+
+//     });
+
+//     const results = await new Promise((resolve, reject) => {
+//       db.query("SELECT * FROM employee", function (err, results) {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(results);
+//         }
+//       });
+//     });
+//     results.forEach((employee) => {
+//       employeeTable.push([
+//         employee.id,
+//         employee.fname,
+//         employee.lname,
+//         employee.role_id,
+//         employee.manager_id,
+//       ]);
+//     });
+//     console.log(employeeTable.toString());
+//     prompt();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// async function addDepartment() {
+//   try {
+//     const employeeTable = new Table({
+//       head: ['id', 'fname', 'lname', 'role_id', 'department_id']
+
+//     });
+
+//     const results = await new Promise((resolve, reject) => {
+//       db.query("SELECT * FROM employee", function (err, results) {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(results);
+//         }
+//       });
+//     });
+//     results.forEach((employee) => {
+//       employeeTable.push([
+//         employee.id,
+//         employee.fname,
+//         employee.lname,
+//         employee.role_id,
+//         employee.manager_id,
+//       ]);
+//     });
+//     console.log(employeeTable.toString());
+//     prompt();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 function prompt() {
   inquirer
@@ -119,6 +159,17 @@ function prompt() {
         viewAllRoles();
       } else if (response.selection == "view all employees") {
         viewAllEmployees();
+      } else if(response.selection == 'add a department'){
+        inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "department",
+            message: "What is the name of the department?",
+          },
+        ]).then((response) => {
+          console.log(response.department)
+        })
       }
     })
     .catch((error) => {
@@ -126,8 +177,6 @@ function prompt() {
       res.status(500).send("Error processing selection");
     });
 }
-
-prompt();
 
 app.use((req, res) => {
   res.status(404).end();
