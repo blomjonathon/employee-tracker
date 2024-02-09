f315ba0cd3a0c3f82a6f4e76b9e41c782c8f2fc1
Jonathon Blom <blomjonathon@gmail.com>
2024-02-09 16:03:07 -0600

    working on function to add role

--- server.js
+++ server.js
@@ -1,138 +1,135 @@
-const figlet = require("figlet");
 const Table = require("cli-table");
 const inquirer = require("inquirer");
-const express = require("express");
 const mysql = require("mysql2");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "jonathon123",
+    database: "departments_db",
+  },
+  console.log(`Connected to the departments_db database.`)
+);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+async function viewAllDepartments() {
+  try {
+    const departmentTable = new Table({
+      head: ["id", "name"],
+    });
 
-prompt()
+    const results = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM department", function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    results.forEach((department) => {
+      departmentTable.push([department.id, department.name]);
+    });
+    console.log(departmentTable.toString());
+    prompt();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// const db = mysql.createConnection(
-//   {
-//     host: "localhost",
-//     user: "root",
-//     password: "jonathon123",
-//     database: "departments_db",
-//   },
-//   console.log(`Connected to the departments_db database.`)
-// );
+function getDepartmentNames() {
+  db.query(`SELECT name FROM department`, function (err, results) {
+    if (err) {
+      console.error(err);
+      return callback(err, null);
+    }
+    const departmentNames = results.map(obj => obj.name);
+    console.log(departmentNames)
+  });
+}
 
-// async function viewAllDepartments() {
-//   try {
-//     const departmentTable = new Table({
-//       head: ['id', 'name']
-//     });
+async function viewAllRoles() {
+  try {
+    const roleTable = new Table({
+      head: ["id", "title", "salary", "department_id"],
+    });
 
-//     const results = await new Promise((resolve, reject) => {
-//       db.query("SELECT * FROM department", function (err, results) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(results);
-//         }
-//       });
-//     });
-//     results.forEach((department) => {
-//       departmentTable.push([department.id, department.name]);
-//     });
-//     console.log(departmentTable.toString());
-//     prompt();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// async function viewAllRoles() {
-//   try {
-//     const roleTable = new Table({
-//       head: ['id', 'title', 'salary', 'department_id']
-//     });
+    const results = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM role", function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    results.forEach((role) => {
+      roleTable.push([role.id, role.title, role.salary, role.department_id]);
+    });
+    console.log(roleTable.toString());
+    prompt();
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function viewAllEmployees() {
+  try {
+    const employeeTable = new Table({
+      head: ["id", "fname", "lname", "role_id", "department_id"],
+    });
 
-//     const results = await new Promise((resolve, reject) => {
-//       db.query("SELECT * FROM role", function (err, results) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(results);
-//         }
-//       });
-//     });
-//     results.forEach((role) => {
-//       roleTable.push([role.id, role.title, role.salary, role.department_id]);
-//     });
-//     console.log(roleTable.toString());
-//     prompt();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// async function viewAllEmployees() {
-//   try {
-//     const employeeTable = new Table({
-//       head: ['id', 'fname', 'lname', 'role_id', 'department_id']
+    const results = await new Promise((resolve, reject) => {
+      db.query("SELECT * FROM employee", function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    results.forEach((employee) => {
+      employeeTable.push([
+        employee.id,
+        employee.fname,
+        employee.lname,
+        employee.role_id,
+        employee.manager_id,
+      ]);
+    });
+    console.log(employeeTable.toString());
+    prompt();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-//     });
+function addDepartment(addDepartmentName) {
+  db.query(
+    `INSERT INTO department (name) VALUES ('${addDepartmentName}')`,
+    function (err, results) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Department added successfully!");
+        prompt();
+      }
+    }
+  );
+}
 
-//     const results = await new Promise((resolve, reject) => {
-//       db.query("SELECT * FROM employee", function (err, results) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(results);
-//         }
-//       });
-//     });
-//     results.forEach((employee) => {
-//       employeeTable.push([
-//         employee.id,
-//         employee.fname,
-//         employee.lname,
-//         employee.role_id,
-//         employee.manager_id,
-//       ]);
-//     });
-//     console.log(employeeTable.toString());
-//     prompt();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// async function addDepartment() {
-//   try {
-//     const employeeTable = new Table({
-//       head: ['id', 'fname', 'lname', 'role_id', 'department_id']
-
-//     });
-
-//     const results = await new Promise((resolve, reject) => {
-//       db.query("SELECT * FROM employee", function (err, results) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(results);
-//         }
-//       });
-//     });
-//     results.forEach((employee) => {
-//       employeeTable.push([
-//         employee.id,
-//         employee.fname,
-//         employee.lname,
-//         employee.role_id,
-//         employee.manager_id,
-//       ]);
-//     });
-//     console.log(employeeTable.toString());
-//     prompt();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+function addRole(addRole, addSalary, addRoleDepartment) {
+  db.query(
+    `INSERT INTO role (title, salary, department_id) VALUES ('${addRole}', '${addSalary}', '${addRoleDepartment}')`,
+    function (err, results) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("role added successfully!");
+        prompt();
+      }
+    }
+  );
+}
 
 function prompt() {
   inquirer
@@ -159,17 +156,45 @@ function prompt() {
         viewAllRoles();
       } else if (response.selection == "view all employees") {
         viewAllEmployees();
-      } else if(response.selection == 'add a department'){
+      } else if (response.selection == "add a department") {
         inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "department",
-            message: "What is the name of the department?",
-          },
-        ]).then((response) => {
-          console.log(response.department)
-        })
+          .prompt([
+            {
+              type: "input",
+              name: "newDepartment",
+              message: "What is the name of the department?",
+            },
+          ])
+          .then((response) => {
+            addDepartment(response.newDepartment);
+          });
+      } else if (response.selection == "add a role") {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "newRole",
+              message: "What is the name of the role?",
+            },
+            {
+              type: "input",
+              name: "newSalary",
+              message: "What is the salary of this role?",
+            },
+            {
+              type: "list",
+              name: "newRoleDepartment",
+              message: "Which department will this role be in?",
+              choices: getDepartmentNames(),
+            },
+          ])
+          .then((response) => {
+            addRole(
+              response.newRole,
+              response.newSalary,
+              response.newRoleDepartment
+            );
+          });
       }
     })
     .catch((error) => {
@@ -177,32 +202,5 @@ function prompt() {
       res.status(500).send("Error processing selection");
     });
 }
-
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-// function createGraphic() {
-//   figlet("Employee", "Doom", function (err, data) {
-//     if (err) {
-//       console.log("Something went wrong...");
-//       console.dir(err);
-//       return;
-//     }
-//     console.log(data);
-//   });
-//   figlet("Manager", "Doom", function (err, data) {
-//     if (err) {
-//       console.log("Something went wrong...");
-//       console.dir(err);
-//       return;
-//     }
-//     console.log(data);
-//   });
-// }
-
-// createGraphic()
+// prompt()
+console.log(getDepartmentNames());
